a52c9dfa6d3e3cca98e3074346a092b61f89da96
Unna <unna.arunachalam@nottingham.ac.uk>
2014-03-11 11:06:18 +0000

    timeline changes

--- client/views/timeline.js
+++ client/views/timeline.js
@@ -1,15 +1,82 @@
+/**
+ *  Returns a resource from a Media object if it can find one.
+ */
+ function getResource(context){
+  item = Media.findOne({resource:""+context.resource});
+  if(item){
+   
+    return item.resource;
+   
+  } else{
+    return null;
+  }
+}
+
+/**
+ * Returns the resource from the corresponding Media object to "this"
+ */
+ Template.medium.resource = function () {
+   //alert(getResource(this));
+  
+  return getResource(this);
+};
+
+
+
+
 Template.timeline.events({
-  'click input' : function () {
+  'click input' : function (e) {
     // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined'){
-      console.log("Display Timeline");
-      var dataObj = {     "timeline":     {         "headline":"Some Timeline",        "type":"default",    "text":"People say stuff",    "startDate":"2012,1,26",        "date": [            {                "startDate":"2011,12,12",         "endDate":"2012,1,27",                "headline":"Vine",                "text":"<p>Vine Test</p>",                "asset":                {                     "media":"https://vine.co/v/b55LOA1dgJU",                    "credit":"",                    "caption":"hello"                }            }        ]    } };
+   if (typeof console !== 'undefined'){
+     // console.log("Display Timeline");
+     e.preventDefault();
+    currentCrisis = Crises.findOne(Session.get('currentCrisisId'));
+      
+        timelineObj=(Media.findOne({resource: currentCrisis.dossier.media[0].resource}));
+        Session.set('currentMediaItemId', this.resource);
+      
+  	
+
+     
+      var dataObj = {
+    "timeline":
+    {
+       
+        "headline":timelineObj.headline,
+        "type":'default',
+		"startDate":timelineObj.startDate,
+                 "endDate":timelineObj.endDate,
+		"text":timelineObj.text,
+		"asset":
+        {
+            "media":timelineObj.resource,
+             "credit":"",
+            "caption":""
+        },
+        "date": [
+            {
+                "startDate":timelineObj.date[0].startDate,
+                "endDate":timelineObj.date[1].endDate,
+                "headline":timelineObj.date[2].headline,
+                "text":timelineObj.date[3].text,
+                "asset":
+                {
+                    "media":timelineObj.date[4].asset[0].media,
+                    "credit":"",
+                    "caption":""
+                }
+            }
+ 	]
+      }
+
+}
       createStoryJS({
                         type:       'timeline',
-                        width:      '640',
+                        width:      '1000',
                         height:     '480',
                         source: dataObj
       });
     }
   }
 });
+          
